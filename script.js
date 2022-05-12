60189f68e7976aa2041b2861f104c35be0bf5d68
Keon Park <you@example.com>
2022-05-12 12:19:10 -0400

    css

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.querySelector('#search').addEventListener('click', getPokemon);
 
+
 function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
@@ -12,29 +13,30 @@ function getPokemon(pm) {
   const name = document.querySelector('#pokemonName').value;
   const pokemonName = lowerCaseName(name);
 
-  fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
     .then((response) => response.json())
     .then((data) => {
       document.querySelector('.pokemonBox').innerHTML = `
     <div>
-        <img src="${data.sprites.other["official-artwork"].front_default}" 
+        <img src="${data.sprites.other["official-artwork"].front_default}"
         alt="${capitalizeFirstLetter(data.name)}"/>
     </div>
     <div class="pokemonInfo">
         <h3>${capitalizeFirstLetter(data.name)}</h3>
-        <p>${data.weight}</p>
-        <p>${data.type}</p>
-        <p>${data.No}</p>
-        
-    </div>
-    `;
+        <br>
+        <p>Weight ${data.weight}</p>
+        <br>
+        <p>Height ${data.height}</p>
+        <br>
+        <p>ID # ${data.id}</p>
 
+    </div>`
+    
     })
     .catch((err) => {
+      document.querySelector('.pokemonBox').innerHTML = `
+      <h2>Pokemon Not Found!</h2>`
       console.log('Pokemon not found', err);
     });
-
-  pm.preventdefault();
-
+  pm.preventDefault();
 }
-
